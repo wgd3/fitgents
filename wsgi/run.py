8b8b55324f2ce9fa563a649bd1fbb1914128a51a
Wallace Daniel <wallace.daniel3@gmail.com>
2014-10-08 10:41:04 -0400

    Added login/register templates and database updates. No functionality yet

--- wsgi/run.py
+++ wsgi/run.py
@@ -1,9 +1,14 @@
-import os
+import os, datetime
 from flask import Flask
 from flask import render_template
-from flask import request, session, redirect
+from flask import request, session, redirect, flash, g
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from random import SystemRandom
+from backports.pbkdf2 import pbkdf2_hmac, compare_digest
+from flask.ext.login import UserMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+
 
 app = Flask(__name__)
 
@@ -18,23 +23,92 @@ class User(db.Model):
 	__tablename__ = 'users'
 	id = db.Column('user_id', db.Integer, primary_key=True)
 	name = db.Column(db.String(50))
+	email = db.Column(db.String(50))
 	start_weight = db.Column(db.Float)
 	start_bf = db.Column(db.Float)
 	age = db.Column(db.Integer)
 	history = db.relationship('UserHistory', backref='user', lazy='dynamic')
-	
+	_password = db.Column(db.LargeBinary(120))
+	_salt = db.Column(db.String(120))
+
+	@hybrid_property
+	def password(self):
+		return self._password
+
+	# In order to ensure that passwords are always stored
+	# hashed and salted in our database we use a descriptor
+	# here which will automatically hash our password
+	# when we provide it (i. e. user.password = "12345")
+	@password.setter
+	def password(self, value):
+		# When a user is first created, give them a salt
+		if self._salt is None:
+			self._salt = bytes(SystemRandom().getrandbits(128))
+		self._password = self._hash_password(value)
+
+	def is_valid_password(self, password):
+		"""Ensure that the provided password is valid.
+		We are using this instead of a ``sqlalchemy.types.TypeDecorator``
+		(which would let us write ``User.password == password`` and have the incoming
+		``password`` be automatically hashed in a SQLAlchemy query)
+		because ``compare_digest`` properly compares **all***
+		the characters of the hash even when they do not match in order to
+		avoid timing oracle side-channel attacks."""
+		new_hash = self._hash_password(password)
+		return compare_digest(new_hash, self._password)
+
+	def _hash_password(self, password):
+		pwd = password.encode("utf-8")
+		salt = bytes(self._salt)
+		buff = pbkdf2_hmac("sha512", pwd, salt, iterations=100000)
+		return bytes(buff)
+
 	def __repr__(self):
 		return '<User %r>' % (self.name)
 	
-class UserHistory(db.Model):
-	__tablename__ = 'user_history'
+class FoodHistory(db.Model):
+	__tablename__ = 'food_history'
 	id = db.Column(db.Integer, primary_key=True)
 	user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+	timestamp = db.Column(db.DateTime)
+	
+	
+class SleepHistory(db.Model):
+	__tablename__ = 'sleep_history'
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+	timestamp = db.Column(db.DateTime)
+	
+class ExcerciseHistory(db.Model):
+	__tablename__ = 'excercise_history'
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+	timestamp = db.Column(db.DateTime)
+
+# I assume this is called before each page request
+@app.before_request
+def load_user():
+	if "user_id" in session:
+		user = User.query.filter_by(username=session["user_id"]).first()
+		print "Found user in session: %s" % user.name
+	else:
+		user = {"name": "Guest"}
+		print "Found no user in session, using Guest"
+		
+	g.user = user
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("dashboard.html")
+	return render_template("dashboard.html")
+
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+@app.route("/register")
+def register():
+	return render_template("register.html")
 
 if __name__ == "__main__":
-    app.run(debug = "True", host="0.0.0.0")
+	app.run(debug = "True", host="0.0.0.0")
