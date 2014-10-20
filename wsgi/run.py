52362d4caf4a30e1c9701e4e847a9f60ab1cb55a
Wallace Daniel <wallace.daniel3@gmail.com>
2014-10-20 18:00:33 -0400

    Added javascript for the statistics page

--- wsgi/run.py
+++ wsgi/run.py
@@ -820,7 +820,24 @@ def addbodylog():
 
 @app.route("/statistics")
 def statistics():
-	return render_template("statistics.html")
+	if "user_id" in session:
+		try:
+			user = User.query.get(session['user_id'])
+			
+			g.user.foodlog = user.food_history.all()
+			g.user.sleeplog = user.sleep_history.all()
+			g.user.bodylog = user.body_history.all()
+			
+			return render_template("statistics.html")
+			
+			
+		except Exception as e:
+			print str(e)
+			flash("There was a problem grabbing user data from the database.", "warning")
+			return redirect(url_for("statistics"))
+	else:
+		flash("You must be logged in to see your statistics", "info")
+		return render_template("statistics.html")
 
 @app.route("/profile")
 def profile():
@@ -973,6 +990,35 @@ def emailTest():
 		flash("Env variables aren't set properly for the mail system", "warning")
 		return redirect(url_for("index"))
 
+###########################################
+# Statistic methods
+###########################################
+
+def average(x):
+	assert len(x) > 0
+	return float(sum(x)) / len(x)
+
+def pearson_def(x, y):
+	assert len(x) == len(y)
+	n = len(x)
+	assert n > 0
+	avg_x = average(x)
+	avg_y = average(y)
+	diffprod = 0
+	xdiff2 = 0
+	ydiff2 = 0
+	for idx in range(n):
+		xdiff = x[idx] - avg_x
+		ydiff = y[idx] - avg_y
+		diffprod += xdiff * ydiff
+		xdiff2 += xdiff * xdiff
+		ydiff2 += ydiff * ydiff
+
+	return diffprod / math.sqrt(xdiff2 * ydiff2)
+
+########################################################
+# Main run code 
+########################################################
 
 if __name__ == "__main__":
 	app.run(debug = "True", host="0.0.0.0")
--- wsgi/static/js/statlog.js
+++ wsgi/static/js/statlog.js
@@ -1,15 +1,100 @@
 $(document).ready( function() {
 	
-	var statLog = $(".statlog-background");
+	var loadingRow = $("#statLoadRow");
+	var statProgressBar = $("#statLoadProgress");
+	var loadingHeader = $("#statLoadHeader");
 	
-	var animationSec = 3;
-	var animationProgress = 0;
-	var animationEnd = .5;
-	var animationSpeed = (animationEnd/animationSec);
+	// The hope is to have 6 main "check points" for the loading of the page: 
+	// Body/Sleep/Food trends
+	// Comparing the data to one another: body vs sleep/body vs food/food vs sleep
+	
+	// Set headers and header index
+	
+	var headers = ["Initializing...",
+					"Analyzing body trends...",
+					"Analyzing sleep trends...",
+					"Analyzing food trends...",
+					"Comparing body and sleep patterns...",
+					"Comparing body and food patterns...",
+					"Comparing food and sleep patterns..."]
+	var headerIndex = 0;
+	
+	// Start by setting the loading bar header
+	loadingHeader.text(headers[headerIndex]);
+	
+	// Start with the body (and add in delay)
+	headerIndex += 1;
+	console.log("Header index: " + String(headerIndex));
+	
+	loadingHeader.text(headers[headerIndex]);
+	console.log("Updating header text to: " + headers[headerIndex]);
+	incrementLoadBar(headerIndex, headers.length, statProgressBar);
 
+	// Kick off the animation and stat loading
+	analyzeBody();
 	
-	while (animationProgress <= animationEnd) {
-		statLog.css({"background": "linear-gradient(to right, rgba(223, 105, 26, animationProgress), rgba(223, 105, 26, animationProgress+animationSpeed))"});
-		setTimeout(function(){animationProgress += animationSpeed}, animationSpeed);
-	}
+	function updateProgressBar() {
+		headerIndex += 1;
+		console.log("Header index: " + String(headerIndex));
+		
+		loadingHeader.text(headers[headerIndex]);
+		console.log("Updating header text to: " + headers[headerIndex]);
+		
+		incrementLoadBar(headerIndex, headers.length, statProgressBar);
+	};
+
+	function incrementLoadBar() {
+		var percentage = (headerIndex / headers.length) * 100;
+		console.log("Increasing progress bar completion to " + String(percentage));
+		
+		statProgressBar.css({"width": String(percentage)+"%"});
+		statProgressBar.text(String(parseInt(percentage))+"%");
+		
+		return true;
+	};
+	
+	function analyzeBody() {
+		console.log("analyzeBody called, starting")
+		setTimeout(function() {
+			updateProgressBar();
+			analyzeSleep();
+		}, 1500);
+	};
+	
+	function analyzeSleep() {
+		console.log("analyzeSleep called, starting")
+		setTimeout(function() {
+			updateProgressBar();
+			analyzeFood();
+		}, 1500);
+	};
+	
+	function analyzeFood() {
+		console.log("analyzeFood called, starting")
+		setTimeout(function() {
+			updateProgressBar();
+			compareBodySleep();
+		}, 1500);
+	};
+	
+	function compareBodySleep(){
+		console.log("compareBodySleep called, starting")
+		setTimeout(function() {
+			updateProgressBar();
+			compareBodyFood();
+		}, 1500);
+	};
+	
+	function compareBodyFood(){
+		console.log("compareBodyFood called, starting")
+		setTimeout(function() {
+			updateProgressBar();
+			compareSleepFood();
+		}, 1500);
+	};
+	
+	function compareSleepFood(){
+		console.log("compareSleepFood called, starting")
+		updateProgressBar();
+	};
 });
