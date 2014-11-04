3cf4b347840915a9d8514e3f9bd350c89cdf9dc0
Wallace Daniel <wallace.daniel3@gmail.com>
2014-11-04 15:35:27 -0500

    Added API to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='FitGents',
       author='Wallace Daniel',
       author_email='wallace.daniel3@me.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask==0.10.1', 'Flask-Login==0.2.11', 'Flask-Mail==0.9.1', 'Flask-SQLAlchemy==2.0', 'Jinja2==2.7.3', 'MarkupSafe==0.23', 'SQLAlchemy==0.9.7', 'Tempita==0.5.2', 'Werkzeug==0.9.6', 'backports.pbkdf2', 'blinker==1.3', 'decorator==3.4.0', 'itsdangerous==0.24', 'pbr==0.10.0', 'psycopg2==2.5.4', 'six==1.8.0', 'sqlalchemy-migrate==0.9.2', 'sqlparse==0.1.12', 'wsgiref==0.1.2'],
+      install_requires=['Flask==0.10.1', 'Flask-Login==0.2.11', 'Flask-Mail==0.9.1', 'Flask-SQLAlchemy==2.0', 'Jinja2==2.7.3', 'MarkupSafe==0.23', 'SQLAlchemy==0.9.7', 'Tempita==0.5.2', 'Werkzeug==0.9.6', 'backports.pbkdf2', 'blinker==1.3', 'decorator==3.4.0', 'itsdangerous==0.24', 'pbr==0.10.0', 'psycopg2==2.5.4', 'six==1.8.0', 'sqlalchemy-migrate==0.9.2', 'sqlparse==0.1.12', 'wsgiref==0.1.2', 'Flask-Restless'],
      )
